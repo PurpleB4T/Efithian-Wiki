530110ed93b21dc4d4ae09b78d4c525b02d37556
PurpleB4T <119735825+PurpleB4T@users.noreply.github.com>
2025-08-12 16:53:39 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://purpleb4t.github.io/Efithian-Wiki/Homepage",
+    baseUrl: "purpleb4t.github.io/Efithian-Wiki",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
